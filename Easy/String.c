e65efac3e68ad47379b2a45ec485f58dfdb0442f
Giovana <giovanaam@dcc.ufrj.br>
2025-02-21 20:00:37 -0300

    Organizando tudo

---
+++ Easy/String.c
@@ -0,0 +1,9 @@
+//Esse e um codigo que ira receber um nome e imprimir as 4 primeiras letras desse nome
+#include <stdio.h>
+int main()
+{
+//Forma de ler e imprimir um nome, nao e preciso E comercial e nem de for, o nome e String
+    char nome[10];
+    scanf("%s",nome);
+    printf("%s",nome);
+}
